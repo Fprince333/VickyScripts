b39293cebc4dd3295798f7dffbe0a40c11f8dd92
Will Smith <lwilsonsmith@gmail.com>
2019-12-22 13:20:22 -0500

    Adds Play on Netflix script

---
+++ playOnNetflix.js
@@ -0,0 +1,59 @@
+// ==VickiScript==
+// @name Play on Netflix
+// @id heyvicki.play-on-netflix
+// @description Play on Netflix
+// @utterance Ask $invocation to play The Office
+// @permission native
+// ==/VickiScript==
+
+$vs.listen([/^play(\s+.*)?$/], async (req, res, match) => {
+  let target = match[1];
+  if (!target) {
+    const newReq = await res
+      .say("Play what?")
+      .reprompt("What would you like to play?")
+      .sendAndListen();
+
+    target = newReq.utterance;
+  }
+
+  if (target) {
+    const url = "https://www.netflix.com/browse";
+    const tab = await browser.tabs.create({ url });
+    await browser.tabs.executeScript(tab.id, { code: `window.location.href = "https://www.netflix.com/search?q=${target}";` });
+    waitForNewTabUrl(tab.id, `"https://www.netflix.com/search?q=${target}"`, 10000);
+    res.say("Playing");
+  }
+
+  res.enableSessionContinuation();
+  res.send();
+
+});
+
+function delay(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time)
+  });
+}
+
+async function untilDefinedOrTimeout(pollRateMs, timeoutMs, asyncPredicate) {
+  const expirationTime = Date.now() + timeoutMs;
+  while (expirationTime > Date.now()) {
+    await delay(pollRateMs);
+
+    const value = await asyncPredicate();
+    if (value !== undefined) {
+      return value;
+    }
+  }
+}
+
+async function waitForNewTabUrl(tabId, oldUrl, timeout) {
+  const pollRateMs = 100;
+  return untilDefinedOrTimeout(pollRateMs, timeout, async () => {
+    const tab = await browser.tabs.get(tabId);
+    if (tab.url !== oldUrl) {
+      await browser.tabs.executeScript(tab.id, { code: `document.getElementById("title-card-0-0").firstElementChild.firstElementChild.click();document.getElementsByClassName("nf-flat-button-icon-play")[0].click();` });
+    }
+  });
+}
